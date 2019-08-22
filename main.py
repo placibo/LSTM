f641e7a57b922271eea3a771124cb6ff30eba879
Abhishek Shrivastava <abhishekshrivastava051@gmail.com>
2019-08-22 15:45:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd              #for data manipulation
 import matplotlib.pyplot as plt  #for visualization
 
 
-#data given and test
+#data given and test and 1234
 path = r'./input/NationalNames.csv'
 data = pd.read_csv(path)
 
